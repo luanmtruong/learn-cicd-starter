82a02a16e61d8b709ec88d74b0d0f160cc6913fe
Luan Truong Minh <89016759+luanmtruong@users.noreply.github.com>
2025-06-13 13:39:47 +0700

    update readme (#1)
    * update readme
    
    * add ci.yml
    
    * update ci.yml
    
    * add unit test
    
    * change CI to run unit test
    
    * fix unit test
    
    * add -cover option to unit test
    
    * add badge to README file

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	// Test: Valid API key
+	headers := http.Header{
+		"Authorization": []string{"ApiKey testkey"},
+	}
+	key, err := GetAPIKey(headers)
+	require.NoError(t, err)
+	require.NotNil(t, key)
+	assert.Equal(t, "testkey", key)
+
+	// Test: No Authorization header
+	headers = http.Header{}
+	key, err = GetAPIKey(headers)
+	require.EqualError(t, err, ErrNoAuthHeaderIncluded.Error())
+	assert.Empty(t, key)
+
+	// Test: wrong prefix
+	headers = http.Header{
+		"Authorization": []string{"Bearer token"},
+	}
+	key, err = GetAPIKey(headers)
+	require.EqualError(t, err, "malformed authorization header")
+	assert.Empty(t, key)
+
+	// Test: missing key
+	headers = http.Header{
+		"Authorization": []string{"ApiKey"},
+	}
+	key, err = GetAPIKey(headers)
+	require.EqualError(t, err, "malformed authorization header")
+	assert.Empty(t, key)
+
+	// Test: empty value
+	headers = http.Header{
+		"Authorization": []string{"-"},
+	}
+	key, err = GetAPIKey(headers)
+	require.EqualError(t, err, "malformed authorization header")
+	assert.Empty(t, key)
+}
+
