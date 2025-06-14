5720da71a2953ca01c061c87d774a643b2c5000d
s4ukk <luanmtruong15@gmail.com>
2025-06-14 19:02:11 +0700

    fix security problem

--- json.go
+++ json.go
@@ -32,7 +32,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		log.Printf("Error writing JSON: %s", err)
-		w.WriteHeader(500)
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
